89ffd09ddb7309f18382081c608700755e1df636
Rocio <roociow@gmail.com>
2026-02-23 21:06:37 -0300

    gateway and mscv running in docker-compose

--- msvc-notification/src/main/java/com/todoapp/msvc_notification/dto/request/NotificationRequestDTO.java
+++ msvc-notification/src/main/java/com/todoapp/msvc_notification/dto/request/NotificationRequestDTO.java
@@ -1,7 +1,7 @@
 package com.todoapp.msvc_notification.dto.request;
 
 public record NotificationRequestDTO(
-        String id,
+    String id,
     String message,
     Long userId
 ) {
--- msvc-notification/src/main/java/com/todoapp/msvc_notification/entity/Notification.java
+++ msvc-notification/src/main/java/com/todoapp/msvc_notification/entity/Notification.java
@@ -4,6 +4,7 @@ import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
